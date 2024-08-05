108ebfd389372cba8a176f49f809070585aa7938
csjurado <carlos.santiago.jurado.bazan@gmail.com>
2024-08-04 22:59:26 -0500

    second commit

--- src/components/hooks/useWeather.ts
+++ src/components/hooks/useWeather.ts
@@ -43,7 +43,7 @@ export default function useWeather() {
     setLoading(true);
     setWeather(initalState);
     try {
-      const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
+      const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
       const { data } = await axios(geoUrl);
 
       if (!data[0]) {
